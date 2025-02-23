1358e79b613e82148c57171eaade954fe4ae1e22
Martin Olivesten <marol450@student.liu.se>
2025-02-23 03:21:20 +0100

    Update xp-farm after testing

--- xp-farm.lua
+++ xp-farm.lua
@@ -1,11 +1,11 @@
 
 
 function main()
-    t1 = peripheral.find("create:fluid_tank_0") or error("No fluid tank 0!")
-    t2 = peripheral.find("create:fluid_tank_1") or error("No fluid tank 1!")
+    t1 = peripheral.wrap("create:fluid_tank_0") or error("No fluid tank 0!")
+    t2 = peripheral.wrap("create:fluid_tank_1") or error("No fluid tank 1!")
     m = peripheral.find("monitor") or error("No Monitor!")
     
-    if t1.tanks().name == "create_enchantment_industry:hyper_experience" then
+    if t1.tanks()[1].name == "create_enchantment_industry:hyper_experience" then
         hxp = t1
         xp = t2
     else
@@ -23,9 +23,9 @@ function main()
         m.clear()
         -- For 5x1 monitor
         m.setCursorPos(7,1)
-        m.write(string.format("%d mB XP", xp.tanks().amount))
+        m.write(string.format("%d mB XP", xp.tanks()[1].amount))
         m.setCursorPos(7,2)
-        m.write(string.format("%d mB HXP", hxp.tanks().amount))
+        m.write(string.format("%d mB HXP", hxp.tanks()[1].amount))
         print("Update done")
         sleep(5)
     end
